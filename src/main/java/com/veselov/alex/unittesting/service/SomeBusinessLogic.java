fe82eac1488fbad99116f330be30b50443c14517
Pogorelov Roman <rmk140987@gmail.com>
2019-12-12 15:43:01 +0300

    add mock

--- src/main/java/com/veselov/alex/unittesting/service/SomeBusinessLogic.java
+++ src/main/java/com/veselov/alex/unittesting/service/SomeBusinessLogic.java
@@ -3,8 +3,10 @@ package com.veselov.alex.unittesting.service;
 public class SomeBusinessLogic {
     private SomeDataService service;
 
-    public int calculateSum(int[] ar) {
+
+    public int calculateSumUsingService() {
         int sum = 0;
+        int[] ar = this.service.retrieveData();
         for (int cur :
                 ar) {
             sum += cur;
@@ -12,13 +14,7 @@ public class SomeBusinessLogic {
         return sum;
     }
 
-    public int calculateSumUsingService() {
-        int sum = 0;
-        int[] ar = service.retrieveData();
-        for (int cur :
-                ar) {
-            sum += cur;
-        }
-        return sum;
+    public void setService(SomeDataService service) {
+        this.service = service;
     }
 }
--- src/test/java/com/veselov/alex/unittesting/service/SomeBusinessLogicTest.java
+++ src/test/java/com/veselov/alex/unittesting/service/SomeBusinessLogicTest.java
@@ -3,13 +3,18 @@ package com.veselov.alex.unittesting.service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SomeBusinessLogicTest {
 
     @Test
     void calculateSum() {
         SomeBusinessLogic logic = new SomeBusinessLogic();
-        int actual = logic.calculateSum(new int[]{1, 2, 3});
+        SomeDataService mock = mock(SomeDataService.class);
+        when(mock.retrieveData()).thenReturn(new int[]{1, 2, 3});
+        logic.setService(mock);
+        int actual = logic.calculateSumUsingService();
         int expected = 6;
         assertEquals(expected, actual);
     }
@@ -17,7 +22,10 @@ class SomeBusinessLogicTest {
     @Test
     void calculateZero() {
         SomeBusinessLogic logic = new SomeBusinessLogic();
-        int actual = logic.calculateSum(new int[]{});
+        SomeDataService mock = mock(SomeDataService.class);
+        when(mock.retrieveData()).thenReturn(new int[]{});
+        logic.setService(mock);
+        int actual = logic.calculateSumUsingService();
         int expected = 0;
         assertEquals(expected, actual);
     }
@@ -25,8 +33,11 @@ class SomeBusinessLogicTest {
     @Test
     void calculateOneValue() {
         SomeBusinessLogic logic = new SomeBusinessLogic();
-        int actual = logic.calculateSum(new int[]{1});
-        int expected = 1;
+        SomeDataService mock = mock(SomeDataService.class);
+        when(mock.retrieveData()).thenReturn(new int[]{3});
+        logic.setService(mock);
+        int actual = logic.calculateSumUsingService();
+        int expected = 3;
         assertEquals(expected, actual);
     }
 }
