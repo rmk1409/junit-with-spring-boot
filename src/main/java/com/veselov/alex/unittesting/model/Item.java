3dd3eb6e19dd0c2197b803b18e852c6fbe1a6b16
Pogorelov Roman <rmk140987@gmail.com>
2019-12-13 11:32:03 +0300

    add jpa and h2

--- src/main/java/com/veselov/alex/unittesting/model/Item.java
+++ src/main/java/com/veselov/alex/unittesting/model/Item.java
@@ -1,10 +1,19 @@
 package com.veselov.alex.unittesting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class Item {
+    @Id
     private int id;
     private String name;
     private int price;
     private int quantity;
+
+    @Transient
+    private int value;
 
     public Item(int id, String name, int price, int quantity) {
         this.id = id;
@@ -27,6 +36,14 @@ public class Item {
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     @Override
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into item
+values (1001, 'Item1', 10, 20);
