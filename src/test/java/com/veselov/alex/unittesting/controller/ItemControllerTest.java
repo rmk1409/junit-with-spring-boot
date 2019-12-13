ca0d3b4978a5b9215455cb8bc0aea2c9399dd171
Pogorelov Roman <rmk140987@gmail.com>
2019-12-13 09:36:53 +0300

    add json check

---
+++ src/test/java/com/veselov/alex/unittesting/controller/ItemControllerTest.java
@@ -0,0 +1,34 @@
+package com.veselov.alex.unittesting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ItemController.class)
+public class ItemControllerTest {
+    @Autowired
+    private MockMvc mock;
+
+    @Test
+    public void dummyItem_basic() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/dummy-item")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mock.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+    }
+}
