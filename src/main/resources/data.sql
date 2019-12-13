88c3cf537724961274e0cbd8c6f9070da0156390
Pogorelov Roman <rmk140987@gmail.com>
2019-12-13 12:18:30 +0300

    add testing for web -> service -> spring data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into item values (1001, 'Item1', 11, 21);
 insert into item values (1002, 'Item2', 12, 22);
-insert into item values (1003, 'Item1', 13, 23);
+insert into item values (1003, 'Item3', 13, 23);
--- src/test/java/com/veselov/alex/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/veselov/alex/unittesting/controller/ItemControllerTest.java
@@ -14,6 +14,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,6 +47,22 @@ public class ItemControllerTest {
         MvcResult result = mock.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().json("{id:2,name:Snagk,price:5}"))
+                .andReturn();
+    }
+
+    @Test
+    public void dummyItemDataLayer_basic() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/all-item-from-data-base")
+                .accept(MediaType.APPLICATION_JSON);
+        when(service.retrieveAllItems()).thenReturn(
+                Arrays.asList(
+                        new Item(1001, "Item1", 11, 21),
+                        new Item(1002, "Item2", 12, 22),
+                        new Item(1003, "Item3", 13, 23))
+        );
+        mock.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{id:1001,name:Item1,price:11,quantity:21,value:0},{id:1002,name:Item2,price:12,quantity:22,value:0},{id:1003,name:Item3,price:13,quantity:23,value:0}]"))
                 .andReturn();
     }
 }
